2031bc7cd27bb7855277551141f34e5c133aef03
Ralitsa Velikova <rherince@gmail.com>
2023-11-02 00:56:57 +0200

    Fix some typos and calculate the velocities for the third protocol

--- maneuvering.py
+++ maneuvering.py
@@ -18,7 +18,7 @@ def getVElliptic(a, r):
 
 # orbital transfers
 def hohmannTransfer(apar, atar):
-    print("-------Hohmann transfer-------")
+    print("---------Hohmann transfer----------")
 
     # Maneuver 1
     vpar = getVCircular(apar)
@@ -26,23 +26,26 @@ def hohmannTransfer(apar, atar):
     vtransperigee = getVElliptic(atrans, apar)
     deltaV1 = vtransperigee - vpar
 
-    print("V parking: {0:.2f}km".format(vpar / KM))
-    print("V transfer - perigee: {0:.2f}km/s".format(vtransperigee / KM))
-    print("Delta V1: {0:.2f}km/s".format(deltaV1 / KM))
+    print("V parking: {0:.5f}km/s".format(vpar / KM))
+    print("V transfer - perigee: {0:.5f}km/s".format(vtransperigee / KM))
+    print("Delta V1: {0:.5f}km/s".format(deltaV1 / KM))
 
     # Maneuver 2
     vtransapogee = getVElliptic(atrans, atar)
     vtar = getVCircular(atar)
     deltaV2 = vtar - vtransapogee
 
-    print("V transfer - apogee: {0:.2f}km/s".format(vtransapogee / KM))
-    print("V target: {0:.2f}km/s".format(vtar / KM))
-    print("Delta V2: {0:.2f}km/s".format(deltaV2 / KM))
+    print("V transfer - apogee: {0:.5f}km/s".format(vtransapogee / KM))
+    print("V target: {0:.5f}km/s".format(vtar / KM))
+    print("Delta V2: {0:.5f}km/s".format(deltaV2 / KM))
 
-    print("-----------------------------")
+    deltaV = np.abs(deltaV1) + np.abs(deltaV2);
+    print("Delta V: {0:.5f}km/s".format(deltaV / KM))
+
+    print("-----------------------------------")
 
 def biellipticTransfer(apar, atar, f):
-    print("-------Bi-elliptic transfer-------")
+    print("--------Bi-elliptic transfer-------")
 
     # Maneuver 1
     vpar = getVCircular(apar)
@@ -50,10 +53,10 @@ def biellipticTransfer(apar, atar, f):
     vtrans1perigee = getVElliptic(atrans1, apar)
     deltaV1 = vtrans1perigee - vpar
 
-    print("SMA parking: {0:.2f}km".format(apar / KM))
-    print("SMA transfer 1: {0:.2f}km".format(atrans1 / KM))
-    print("V transfer 1 - perigee: {0:.2f}km/s".format(vtrans1perigee / KM))
-    print("Delta V1: {0:.2f}km/s".format(deltaV1 / KM))
+    print("SMA parking: {0:.5f}km".format(apar / KM))
+    print("SMA transfer 1: {0:.5f}km".format(atrans1 / KM))
+    print("V transfer 1 - perigee: {0:.5f}km/s".format(vtrans1perigee / KM))
+    print("Delta V1: {0:.5f}km/s".format(deltaV1 / KM))
 
     # Maneuver 2
     vtrans1apogee = getVElliptic(atrans1, 2 * atrans1 - apar)
@@ -61,27 +64,36 @@ def biellipticTransfer(apar, atar, f):
     vtrans2apogee = getVElliptic(atrans2, 2 * atrans1 - apar)
     deltaV2 = vtrans2apogee - vtrans1apogee
 
-    print("SMA transfer 2: {0:.2f}km".format(atrans2 / KM))
-    print("V transfer 1 - apogee: {0:.2f}km/s".format(vtrans1apogee / KM))
-    print("V transfer 2 - apogee: {0:.2f}km/s".format(vtrans2apogee / KM))
-    print("Delta V2: {0:.2f}km/s".format(deltaV2 / KM))
+    print("SMA transfer 2: {0:.5f}km".format(atrans2 / KM))
+    print("V transfer 1 - apogee: {0:.5f}km/s".format(vtrans1apogee / KM))
+    print("V transfer 2 - apogee: {0:.5f}km/s".format(vtrans2apogee / KM))
+    print("Delta V2: {0:.5f}km/s".format(deltaV2 / KM))
 
     # Maneuver 3
     vtrans2perigee = getVElliptic(atrans2, atar)
     vtar = getVCircular(atar)
     deltaV3 = vtar - vtrans2perigee
 
-    print("V transfer 2 - perigee: {0:.2f}km".format(vtrans2perigee / KM))
-    print("V target: {0:.2f}km/s".format(vtar / KM))
-    print("Delta V3: {0:.2f}km/s".format(deltaV3 / KM)) 
+    print("V transfer 2 - perigee: {0:.5f}km/s".format(vtrans2perigee / KM))
+    print("V target: {0:.5f}km/s".format(vtar / KM))
+    print("Delta V3: {0:.5f}km/s".format(deltaV3 / KM)) 
 
-    print("-----------------------------")
+    deltaV = np.abs(deltaV1) + np.abs(deltaV2) + np.abs(deltaV3);
+    print("Delta V: {0:.5f}km/s".format(deltaV / KM))
+
+    print("-----------------------------------")
 
 if __name__ == "__main__":
     apar = 7000 * KM
-    atar = 30000 * KM
+    atar = 35000 * KM
+    hohmannTransfer(apar, atar)
+
+    apar = 9000 * KM
+    atar = 35000 * KM
     hohmannTransfer(apar, atar)
 
     apar2 = 7000 * KM
     atar2 = 105000 * KM
     biellipticTransfer(apar2, atar2, 2)
+    hohmannTransfer(apar2, atar2)
+    biellipticTransfer(apar2, atar2, 3)
