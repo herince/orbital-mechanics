d529a2a7693e101ce1c1f5fb155eba8af97b7671
Ralitsa Velikova <rherince@gmail.com>
2023-11-12 02:16:32 +0200

    Use better names

--- maneuvering.py
+++ maneuvering.py
@@ -19,22 +19,22 @@ def getVElliptic(a, r):
     return np.sqrt(MU * (2 / r - 1 / a))
 
 # orbital transfers
-def hohmannTransfer(apar, atar):
+def hohmannTransfer(smaParking, smaTarget):
     print("---------Hohmann transfer----------")
 
     # Maneuver 1
-    vpar = getVCircular(apar)
-    atrans = (apar + atar) / 2
-    vtransperigee = getVElliptic(atrans, apar)
-    deltaV1 = vtransperigee - vpar
+    vParking = getVCircular(smaParking)
+    smaTransfer = (smaParking + smaTarget) / 2
+    vTransferPerigee = getVElliptic(smaTransfer, smaParking)
+    deltaV1 = vTransferPerigee - vParking
 
-    print("V parking: {0:.4f}km/s".format(vpar / KM))
-    print("V transfer - perigee: {0:.4f}km/s".format(vtransperigee / KM))
+    print("V parking: {0:.4f}km/s".format(vParking / KM))
+    print("V transfer - perigee: {0:.4f}km/s".format(vTransferPerigee / KM))
     print("Delta V1: {0:.4f}km/s".format(deltaV1 / KM))
 
     # Maneuver 2
-    vtransapogee = getVElliptic(atrans, atar)
-    vtar = getVCircular(atar)
+    vtransapogee = getVElliptic(smaTransfer, smaTarget)
+    vtar = getVCircular(smaTarget)
     deltaV2 = vtar - vtransapogee
 
     print("V transfer - apogee: {0:.4f}km/s".format(vtransapogee / KM))
@@ -46,37 +46,37 @@ def hohmannTransfer(apar, atar):
 
     print("-----------------------------------")
 
-def biellipticTransfer(apar, atar, f):
+def biellipticTransfer(smaParking, smaTarget, f):
     print("--------Bi-elliptic transfer-------")
 
     # Maneuver 1
-    vpar = getVCircular(apar)
-    atrans1 = (atar * f + apar) / 2
-    vtrans1perigee = getVElliptic(atrans1, apar)
-    deltaV1 = vtrans1perigee - vpar
+    vParking = getVCircular(smaParking)
+    smaTrans1 = (smaTarget * f + smaParking) / 2
+    vTrans1Perigee = getVElliptic(smaTrans1, smaParking)
+    deltaV1 = vTrans1Perigee - vParking
 
-    print("SMA parking: {0:.4f}km".format(apar / KM))
-    print("SMA transfer 1: {0:.4f}km".format(atrans1 / KM))
-    print("V transfer 1 - perigee: {0:.4f}km/s".format(vtrans1perigee / KM))
+    print("SMA parking: {0:.4f}km".format(smaParking / KM))
+    print("SMA transfer 1: {0:.4f}km".format(smaTrans1 / KM))
+    print("V transfer 1 - perigee: {0:.4f}km/s".format(vTrans1Perigee / KM))
     print("Delta V1: {0:.4f}km/s".format(deltaV1 / KM))
 
     # Maneuver 2
-    vtrans1apogee = getVElliptic(atrans1, 2 * atrans1 - apar)
-    atrans2 = (2 * atrans1 - apar + atar) / 2
-    vtrans2apogee = getVElliptic(atrans2, 2 * atrans1 - apar)
-    deltaV2 = vtrans2apogee - vtrans1apogee
+    vTrans1Apogee = getVElliptic(smaTrans1, 2 * smaTrans1 - smaParking)
+    smaTrans2 = (2 * smaTrans1 - smaParking + smaTarget) / 2
+    vTrans2Apogee = getVElliptic(smaTrans2, 2 * smaTrans1 - smaParking)
+    deltaV2 = vTrans2Apogee - vTrans1Apogee
 
-    print("SMA transfer 2: {0:.4f}km".format(atrans2 / KM))
-    print("V transfer 1 - apogee: {0:.4f}km/s".format(vtrans1apogee / KM))
-    print("V transfer 2 - apogee: {0:.4f}km/s".format(vtrans2apogee / KM))
+    print("SMA transfer 2: {0:.4f}km".format(smaTrans2 / KM))
+    print("V transfer 1 - apogee: {0:.4f}km/s".format(vTrans1Apogee / KM))
+    print("V transfer 2 - apogee: {0:.4f}km/s".format(vTrans2Apogee / KM))
     print("Delta V2: {0:.4f}km/s".format(deltaV2 / KM))
 
     # Maneuver 3
-    vtrans2perigee = getVElliptic(atrans2, atar)
-    vtar = getVCircular(atar)
-    deltaV3 = vtar - vtrans2perigee
+    vTrans2Perigee = getVElliptic(smaTrans2, smaTarget)
+    vtar = getVCircular(smaTarget)
+    deltaV3 = vtar - vTranP2perigee
 
-    print("V transfer 2 - perigee: {0:.4f}km/s".format(vtrans2perigee / KM))
+    print("V transfer 2 - perigee: {0:.4f}km/s".format(vTrans2Perigee / KM))
     print("V target: {0:.4f}km/s".format(vtar / KM))
     print("Delta V3: {0:.4f}km/s".format(deltaV3 / KM)) 
 
@@ -85,24 +85,24 @@ def biellipticTransfer(apar, atar, f):
 
     print("-----------------------------------")
 
-def phaseTransfer(a, gammaInit, gammaTar, n = 1):
+def phaseTransfer(sma, gammaInit, gammaTar, n = 1):
     print("-----------Phase transfer----------")
 
     phase = gammaInit - gammaTar
-    parkingPeriod = 2 * math.pi * np.sqrt((a ** 3) / MU)
+    parkingPeriod = 2 * math.pi * np.sqrt((sma ** 3) / MU)
     transferTime = (phase / 360) * parkingPeriod
     phasePeriod = parkingPeriod + transferTime / n
-    aTransfer = (MU * ((phasePeriod / (2 * math.pi)) ** 2)) ** (1 / 3)
+    smaTransfer = (MU * ((phasePeriod / (2 * math.pi)) ** 2)) ** (1 / 3)
 
     print("Phase: {0:.4f}deg".format(phase))
     print("Parking period: {0:.4f}h".format(parkingPeriod / H))
     print("Transfer time: {0:.4f}h".format(transferTime / H))
     print("Phase period: {0:.4f}h".format(phasePeriod / H))
-    print("SMA transfer: {0:.4f}km".format(aTransfer / KM))
+    print("SMA transfer: {0:.4f}km".format(smaTransfer / KM))
 
     # Calculate the velocities
-    vParking = getVCircular(a)
-    vPhasePerigee = getVElliptic(aTransfer, a)
+    vParking = getVCircular(sma)
+    vPhasePerigee = getVElliptic(smaTransfer, sma)
 
     print("Parking V: {0:.4f}km/s".format(vParking / KM))
     print("Phase transfer V - perigee: {0:.4f}km/s".format(vPhasePerigee / KM)) 
@@ -115,13 +115,13 @@ def phaseTransfer(a, gammaInit, gammaTar, n = 1):
     print("Delta V2: {0:.4f}km/s".format(deltaV2 / KM)) 
     print("-----------------------------------")
 
-def inclinationChange(a, alpha):
+def inclinationChange(sma, alpha):
     print("---------Inclination change--------")
 
     alphaInRadians = (alpha * np.pi) / 180
 
     # Speed in the parking orbit
-    vParking = getVCircular(a)
+    vParking = getVCircular(sma)
 
     # Velocities for the two impulsive burns
     deltaV1 = vParking * np.sin(alphaInRadians)
@@ -136,26 +136,26 @@ def inclinationChange(a, alpha):
     print("-----------------------------------")
 
 if __name__ == "__main__":
-    apar = 7000 * KM
-    atar = 35000 * KM
-    hohmannTransfer(apar, atar)
+    smaParking = 7000 * KM
+    smaTarget = 35000 * KM
+    hohmannTransfer(smaParking, smaTarget)
 
-    apar2 = 7000 * KM
-    atar2 = 105000 * KM
-    biellipticTransfer(apar2, atar2, 2)
-    hohmannTransfer(apar2, atar2)
-    biellipticTransfer(apar2, atar2, 3)
+    smaParking2 = 7000 * KM
+    smaTarget2 = 105000 * KM
+    biellipticTransfer(smaParking2, smaTarget2, 2)
+    hohmannTransfer(smaParking2, smaTarget2)
+    biellipticTransfer(smaParking2, smaTarget2, 3)
 
-    apar3 = 42164 * KM
+    smaParking3 = 42164 * KM
     gammaInit = 285
     gammaTar = 145
-    phaseTransfer(apar3, gammaInit, gammaTar)
-    phaseTransfer(apar3, gammaInit, gammaTar, 2)
-    phaseTransfer(apar3, gammaInit, gammaTar, 9)
+    phaseTransfer(smaParking3, gammaInit, gammaTar)
+    phaseTransfer(smaParking3, gammaInit, gammaTar, 2)
+    phaseTransfer(smaParking3, gammaInit, gammaTar, 9)
 
-    apar4 = 7200 * KM
+    smaParking4 = 7200 * KM
     alpha = 30
-    inclinationChange(apar4, alpha)
+    inclinationChange(smaParking4, alpha)
 
-    apar5 = 30000 * KM
-    inclinationChange(apar5, alpha)
+    smaParking5 = 30000 * KM
+    inclinationChange(smaParking5, alpha)
